97844d6b4f21f7f5862f437e3dd1ed642b8053eb
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-27 21:30:36 -0500

    updates schema and seed files

--- db/schema.sql
+++ db/schema.sql
@@ -5,17 +5,16 @@ CREATE DATABASE cosmos_db_dev;
 
 -- schema.sql
 
-CREATE TABLE planets (
+CREATE TABLE IF NOT EXISTS planets (
   id INTEGER PRIMARY KEY,
   planetname TEXT NOT NULL,
   description TEXT,
-  discovery_order INTEGER,
   date_of_discovery DATE,
-  is_current_planet BOOLEAN DEFAULT 1,
+  is_current_planet BOOLEAN DEFAULT TRUE,
   diameter_km REAL,
   mass_kg REAL,
   avg_temperature_celsius REAL,
-  planet_picture TEXT, 
-  CONSTRAINT unique_name UNIQUE (planetname),
-  CONSTRAINT positive_discovery_order CHECK (discovery_order >= 0)
+  planet_picture TEXT,
+  CONSTRAINT unique_name UNIQUE (planetname)
+  CONSTRAINT is_current_planet CHECK (is_current_planet = TRUE)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -2,28 +2,37 @@
 
 -- seed.sql
 
-INSERT INTO planets (id, planetname, description, discovery_order, date_of_discovery, is_current_planet, diameter_km, mass_kg, avg_temperature_celsius) VALUES
-(1, 'Mercury', 'The smallest planet in our solar system and the closest to the Sun.', 1, '2022-01-01', 1, 4879, 3.3e23, 167),
-(2, 'Venus', 'Known for its thick atmosphere and runaway greenhouse effect.', 2, '2022-02-01', 1, 12104, 4.87e24, 464),
-(3, 'Earth', 'Our home planet, known for its diverse ecosystems and abundant life.', 3, '2022-03-01', 1, 12742, 5.97e24, 15),
-(4, 'Mars', 'Often called the Red Planet due to its reddish appearance.', 4, '2022-04-01', 1, 6779, 6.39e23, -80),
-(5, 'Jupiter', 'The largest planet in our solar system, known for its Great Red Spot.', 5, '2022-05-01', 1, 139820, 1.9e27, -145),
-(6, 'Saturn', 'Famous for its stunning ring system made of ice particles.', 6, '2022-06-01', 1, 116460, 5.68e26, -178),
-(7, 'Uranus', 'A gas giant with a unique rotation—its axis is tilted almost perpendicular to its orbit.', 7, '2022-07-01', 1, 50724, 8.68e25, -224),
-(8, 'Neptune', 'The farthest known planet in our solar system, with a dynamic and stormy atmosphere.', 8, '2022-08-01', 1, 49244, 1.02e26, -214),
-(9, 'Pluto', 'Dwarf planet located in the Kuiper Belt, known for its heart-shaped feature.', 9, '2022-09-01', 1, 2376, 1.31e22, -230),
-(10, 'Eris', 'Dwarf planet and one of the most massive known dwarf planets.', 10, '2022-10-01', 1, 2326, 1.67e22, -240),
-(11, 'Ceres', 'The largest object in the asteroid belt, now classified as a dwarf planet.', 11, '2022-11-01', 1, 590, 9.39e20, -105),
-(12, 'Haumea', 'Dwarf planet with an elongated shape, located in the Kuiper Belt.', 12, '2022-12-01', 1, 2322, 4.01e21, -241),
-(13, 'Makemake', 'Dwarf planet in the Kuiper Belt, known for its reddish color.', 13, '2023-01-01', 1, 1500, 3.14e21, -243),
-(14, 'Io', 'One of Jupiters largest moons, known for its volcanic activity.', 14, '2023-02-01', 1, 3643, 8.93e22, -130),
-(15, 'Europa', 'Jupiters moon with a subsurface ocean, considered a key target for astrobiology.', 15, '2023-03-01', 1, 3122, 4.8e22, -160),
-(16, 'Ganymede', 'Largest moon in our solar system and a moon of Jupiter.', 16, '2023-04-01', 1, 5262, 1.48e23, -150),
-(17, 'Callisto', 'A moon of Jupiter with a heavily cratered surface.', 17, '2023-05-01', 1, 4800, 1.08e23, -145),
-(18, 'Titan', 'Saturns largest moon, known for its thick atmosphere and lakes of liquid methane.', 18, '2023-06-01', 1, 5150, 1.35e23, -179),
-(19, 'Enceladus', 'A moon of Saturn with geysers of water vapor erupting from its surface.', 19, '2023-07-01', 1, 504, 1.08e20, -201),
-(20, 'Triton', 'Neptunes largest moon, with a retrograde orbit and geysers of nitrogen.', 20, '2023-08-01', 1, 2707, 2.14e22, -235),
-(21, 'Charon', 'Plutos largest moon, forming a binary system with Pluto.', 21, '2023-09-01', 1, 1208, 1.59e21, -220),
+-- seed.sql
+
+INSERT INTO planets (id, planetname, description, date_of_discovery, is_current_planet, diameter_km, mass_kg, avg_temperature_celsius, planet_picture) VALUES
+(1, 'Mercury', 'The smallest planet in our solar system and the closest to the Sun.', '0000-01-01', true, 4879, 3.3e23, 167, 'https://example.com/mercury.jpg'),
+(2, 'Venus', 'Known for its thick atmosphere and runaway greenhouse effect.', '0000-01-01', true, 12104, 4.87e24, 464, 'https://example.com/venus.jpg'),
+(3, 'Earth', 'Our home planet, known for its diverse ecosystems and abundant life.', '0000-01-01', true, 12742, 5.97e24, 15, 'https://example.com/earth.jpg'),
+(4, 'Mars', 'Often called the Red Planet due to its reddish appearance.', '0000-01-01', true, 6779, 6.39e23, -80, 'https://example.com/mars.jpg'),
+(5, 'Jupiter', 'The largest planet in our solar system, known for its Great Red Spot.', '0000-01-01', true, 139820, 1.9e27, -145, 'https://example.com/jupiter.jpg'),
+(6, 'Saturn', 'Famous for its stunning ring system made of ice particles.', '0000-01-01', true, 116460, 5.68e26, -178, 'https://example.com/saturn.jpg'),
+(7, 'Uranus', 'A gas giant with a unique rotation—its axis is tilted almost perpendicular to its orbit.', '0000-01-01', true, 50724, 8.68e25, -224, 'https://example.com/uranus.jpg'),
+(8, 'Neptune', 'The farthest known planet in our solar system, with a dynamic and stormy atmosphere.', '0000-01-01', true, 49244, 1.02e26, -214, 'https://example.com/neptune.jpg'),
+(9, 'Pluto', 'Dwarf planet located in the Kuiper Belt, known for its heart-shaped feature.', '1930-02-18', true, 2376, 1.31e22, -230, 'https://example.com/pluto.jpg'),
+(10, 'Eris', 'Dwarf planet and one of the most massive known dwarf planets.', '2005-01-05', true, 2326, 1.67e22, -240, 'https://example.com/eris.jpg'),
+(11, 'Ceres', 'The largest object in the asteroid belt, now classified as a dwarf planet.', '1801-01-01', true, 590, 9.39e20, -105, 'https://example.com/ceres.jpg'),
+(12, 'Haumea', 'Dwarf planet with an elongated shape, located in the Kuiper Belt.', '2004-12-28', true, 2322, 4.01e21, -241, 'https://example.com/haumea.jpg'),
+(13, 'Makemake', 'Dwarf planet in the Kuiper Belt, known for its reddish color.', '2005-03-31', true, 1500, 3.14e21, -243, 'https://example.com/makemake.jpg'),
+(14, 'Io', 'One of Jupiters largest moons, known for its volcanic activity.', '1610-01-08', true, 3643, 8.93e22, -130, 'https://example.com/io.jpg'),
+(15, 'Europa', 'Jupiters moon with a subsurface ocean, considered a key target for astrobiology.', '1610-01-08', true, 3122, 4.8e22, -160, 'https://example.com/europa.jpg'),
+(16, 'Ganymede', 'Largest moon in our solar system and a moon of Jupiter.', '1610-01-08', true, 5262, 1.48e23, -150, 'https://example.com/ganymede.jpg'),
+(17, 'Callisto', 'A moon of Jupiter with a heavily cratered surface.', '1610-01-08', true, 4800, 1.08e23, -145, 'https://example.com/callisto.jpg'),
+(18, 'Titan', 'Saturns largest moon, known for its thick atmosphere and lakes of liquid methane.', '1655-03-25', true, 5150, 1.35e23, -179, 'https://example.com/titan.jpg'),
+(19, 'Enceladus', 'A moon of Saturn with geysers of water vapor erupting from its surface.', '1789-08-28', true, 504, 1.08e20, -201, 'https://example.com/enceladus.jpg'),
+(20, 'Triton', 'Neptunes largest moon, with a retrograde orbit and geysers of nitrogen.', '1846-10-10', true, 2707, 2.14e22, -235, 'https://example.com/triton.jpg'),
+(21, 'Charon', 'Plutos largest moon, forming a binary system with Pluto.', '1978-06-22', true, 1208, 1.59e21, -220, 'https://example.com/charon.jpg'),
+(22, 'Eris Moon', 'Dwarf planet Eris has a moon named Dysnomia.', '2005-09-10', true, 0, 0, 0, 'https://example.com/eris_moon.jpg'), 
+
+
+
+
+
+
 
 
 
